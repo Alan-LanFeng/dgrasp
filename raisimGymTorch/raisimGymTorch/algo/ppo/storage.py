e904e3f7d66db4a9895d30977ddbe400c5c6292e
lafeng <fenglan18@outlook.com>
2023-03-24 16:15:49 +0100

    debug gae

--- raisimGymTorch/raisimGymTorch/algo/ppo/storage.py
+++ raisimGymTorch/raisimGymTorch/algo/ppo/storage.py
@@ -7,15 +7,13 @@ from raisimGymTorch.helper.utils import first_nonzero
 class RolloutStorage:
     def __init__(self, num_envs, num_transitions_per_env, actor_obs_shape, critic_obs_shape, actions_shape, device):
         self.device = device
-        self.num_envs = num_envs
+
         # Core
         self.critic_obs = np.zeros([num_transitions_per_env, num_envs, *critic_obs_shape], dtype=np.float32)
         self.actor_obs = np.zeros([num_transitions_per_env, num_envs, *actor_obs_shape], dtype=np.float32)
         self.rewards = np.zeros([num_transitions_per_env, num_envs, 1], dtype=np.float32)
         self.actions = np.zeros([num_transitions_per_env, num_envs, *actions_shape], dtype=np.float32)
         self.dones = np.zeros([num_transitions_per_env, num_envs, 1], dtype=bool)
-        self.episode_starts = np.zeros([num_transitions_per_env, num_envs, 1], dtype=bool)
-        self._last_episode_starts = np.ones([1, num_envs, 1], dtype=bool)
         self.mask = np.ones([num_transitions_per_env, num_envs, 1],dtype=bool)
         # For PPO
         self.actions_log_prob = np.zeros([num_transitions_per_env, num_envs, 1], dtype=np.float32)
@@ -52,61 +50,35 @@ class RolloutStorage:
         self.sigma[self.step] = sigma
         self.rewards[self.step] = rewards.reshape(-1, 1)
         self.dones[self.step] = dones.reshape(-1, 1)
-        self.episode_starts[self.step] = self._last_episode_starts
         self.actions_log_prob[self.step] = actions_log_prob.reshape(-1, 1)
-        self._last_episode_starts = dones.reshape(1,-1, 1)
         self.step += 1
 
     def clear(self):
-        self._last_episode_starts = np.ones([1, self.num_envs, 1], dtype=bool)
         self.step = 0
 
     def compute_returns(self, last_values, critic, gamma, lam):
         with torch.no_grad():
-            t,a,dim = self.critic_obs.shape
-            for i in range(t):
-                self.values[i] = critic.predict(torch.from_numpy(self.critic_obs[i]).to(self.device)).cpu().numpy()
-            #self.values = critic.predict(torch.from_numpy(self.critic_obs).to(self.device)).cpu().numpy()
+            self.values = critic.predict(torch.from_numpy(self.critic_obs).to(self.device)).cpu().numpy()
 
-        last_values = last_values.cpu().numpy()
-        self.rewards[-1] += gamma * last_values*(1.0 - self.dones[-1])
+        advantage = 0
+        val_with_last = np.concatenate([self.values,last_values.unsqueeze(0).cpu().numpy()],axis=0)
+        a = first_nonzero(self.dones[...,0],0)
+        for i in range(len(a)):
+            if a[i]==-1:continue
+            indx = a[i].item()
+            val_with_last[indx+1:,i]=0
+            self.rewards[indx+1:,i]=0
+            self.mask[indx+1:,i]=False
+            self.dones[indx:, i] = 1
+        for step in reversed(range(self.num_transitions_per_env)):
+            next_values = val_with_last[step + 1]
+            next_is_not_terminal = 1.0 - self.dones[step]
+            delta = self.rewards[step] + next_is_not_terminal * gamma * next_values - val_with_last[step]
+            advantage = delta + next_is_not_terminal * gamma * lam * advantage
+            self.returns[step] = advantage + val_with_last[step]
 
-        last_gae_lam = 0
-        size = self.actions.shape[0]
-        for step in reversed(range(size)):
-            if step == size - 1:
-                #next_non_terminal = 1.0 - self.dones[step]
-                next_non_terminal = 0
-                next_values = last_values
-            else:
-                next_non_terminal = 1.0 - self.episode_starts[step+1]
-                next_values = self.values[step + 1]
-            delta = self.rewards[step] + gamma * next_values * next_non_terminal - self.values[step]
-            last_gae_lam = delta + gamma * lam * next_non_terminal * last_gae_lam
-            self.advantages[step] = last_gae_lam
-        # TD(lambda) estimator, see Github PR #375 or "Telescoping in TD(lambda)"
-        # in David Silver Lecture 4: https://www.youtube.com/watch?v=PnHCvfgC_ZA
-        self.returns = self.advantages + self.values
-
-        # advantage = 0
-        # val_with_last = np.concatenate([self.values,last_values.unsqueeze(0).cpu().numpy()],axis=0)
-        # a = first_nonzero(self.dones[...,0],0)
-        # for i in range(len(a)):
-        #     if a[i]==-1:continue
-        #     indx = a[i].item()
-        #     val_with_last[indx+1:,i]=0
-        #     self.rewards[indx+1:,i]=0
-        #     self.mask[indx+1:,i]=False
-        #     self.dones[indx:, i] = 1
-        # for step in reversed(range(self.num_transitions_per_env)):
-        #     next_values = val_with_last[step + 1]
-        #     next_is_not_terminal = 1.0 - self.dones[step]
-        #     delta = self.rewards[step] + next_is_not_terminal * gamma * next_values - val_with_last[step]
-        #     advantage = delta + next_is_not_terminal * gamma * lam * advantage
-        #     self.returns[step] = advantage + val_with_last[step]
-        #
-        # # Compute and normalize the advantages
-        # self.advantages = self.returns - self.values
+        # Compute and normalize the advantages
+        self.advantages = self.returns - self.values
         self.advantages = (self.advantages - self.advantages.mean()) / (self.advantages.std() + 1e-8)
 
         # Convert to torch variables
